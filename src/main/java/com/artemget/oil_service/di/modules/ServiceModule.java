872aaef2af4d83ba7557619e4bcc6acce6883944
Artem Getmanskii <re910@yandex.ru>
2021-07-03 21:54:42 +0300

    [OS-3] add di context

--- src/main/java/com/artemget/oil_service/di/modules/ServiceModule.java
+++ src/main/java/com/artemget/oil_service/di/modules/ServiceModule.java
@@ -1,9 +1,12 @@
 package com.artemget.oil_service.di.modules;
 
 import com.artemget.oil_service.model.Oil;
+import com.artemget.oil_service.repository.OilRepository;
+import com.artemget.oil_service.repository.RecordRepository;
 import com.artemget.oil_service.repository.UserRepository;
 import com.artemget.oil_service.service.FinderService;
 import com.artemget.oil_service.service.OilFinderService;
+import com.artemget.oil_service.service.OilStoreService;
 import com.artemget.oil_service.service.UserService;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
@@ -22,5 +25,11 @@ public class ServiceModule extends AbstractModule {
     @Singleton
     public UserService provideUserService(UserRepository userRepository) {
         return new UserService(userRepository);
+    }
+
+    @Provides
+    @Singleton
+    public OilStoreService provideOilStoreService(RecordRepository recordRepository, OilRepository oilRepository) {
+        return new OilStoreService(oilRepository, recordRepository);
     }
 }
